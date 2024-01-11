ab032bf2af3562bca35abcf903e8d3fa605da142
Shante <kennedy2110@gmail.com>
2024-01-11 17:35:40 -0500

    Fixed Js. Changed Profile Pic

--- main.js
+++ main.js
@@ -7,6 +7,16 @@
         menuBtn.className = "nav-menu";
       }
     }
+
+// Dark Mode
+let darkToggle = document.querySelector('#darkToggle');
+
+darkToggle.addEventListener('change', ()=> {
+  document.body.classList.toggle('dark');
+});
+
+
+
 /* ----- ADD SHADOW ON NAVIGATION BAR WHILE SCROLLING ----- */
     window.onscroll = function() {headerShadow()};
     function headerShadow() {
@@ -23,7 +33,7 @@
     }
 /* ----- TYPING EFFECT ----- */
    var typingEffect = new Typed(".typedText",{
-      strings : ["Software Engineer ","Designer"],
+      strings : ["Software Engineer ","Graphic Designer"],
       loop : true,
       typeSpeed : 100, 
       backSpeed : 80,
@@ -87,3 +97,24 @@
     })
   }
   window.addEventListener('scroll', scrollActive)
+
+// Scroll to top button
+  // Get the button:
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
